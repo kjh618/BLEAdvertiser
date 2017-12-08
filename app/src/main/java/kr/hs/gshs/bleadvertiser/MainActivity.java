bc79aeb59f7a5c3b333c4d2b18f5ca29793972a6
kjh <kimjihoon6188@daum.net>
2017-12-08 22:05:32 +0900

    Initial commit, make ble advertiser

---
+++ app/src/main/java/kr/hs/gshs/bleadvertiser/MainActivity.java
@@ -0,0 +1,200 @@
+package kr.hs.gshs.bleadvertiser;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.AdvertiseCallback;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+/**
+ * Allows user to start & stop Bluetooth LE Advertising of their device.
+ *
+ * Setup display fragments and ensure the device supports Bluetooth.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_ENABLE_BT = 1;
+
+    /**
+     * Lets user toggle BLE Advertising.
+     */
+    private Switch mSwitch;
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    /**
+     * Listens for notifications that the {@code AdvertiserService} has failed to start advertising.
+     * This Receiver deals with Fragment UI elements and only needs to be active when the Fragment
+     * is on-screen, so it's defined and registered in code instead of the Manifest.
+     */
+    private BroadcastReceiver advertisingFailureReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+
+            mBluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+
+            // Is Bluetooth supported on this device?
+            if (mBluetoothAdapter != null) {
+
+                // Is Bluetooth turned on?
+                if (mBluetoothAdapter.isEnabled()) {
+
+                    // Are Bluetooth Advertisements supported on this device?
+                    if (!mBluetoothAdapter.isMultipleAdvertisementSupported()) {
+
+                        // Bluetooth Advertisements are not supported.
+                        Toast.makeText(this, "Bluetooth Advertisements are not supported on this device.", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                } else {
+
+                    // Prompt user to turn on Bluetooth (logic continues in onActivityResult()).
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+            } else {
+
+                // Bluetooth is not supported.
+                Toast.makeText(this, "Bluetooth is not supported on this device.", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+
+        advertisingFailureReceiver = new BroadcastReceiver() {
+
+            /**
+             * Receives Advertising error codes from {@code AdvertiserService} and displays error messages
+             * to the user. Sets the advertising toggle to 'false.'
+             */
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                int errorCode = intent.getIntExtra(AdvertiserService.ADVERTISING_FAILED_EXTRA_CODE, -1);
+
+                mSwitch.setChecked(false);
+
+                String errorMessage = "Start advertising failed: ";
+                switch (errorCode) {
+                    case AdvertiseCallback.ADVERTISE_FAILED_ALREADY_STARTED:
+                        errorMessage += "already started.";
+                        break;
+                    case AdvertiseCallback.ADVERTISE_FAILED_DATA_TOO_LARGE:
+                        errorMessage += "data packet exceeded 31 byte limit.";
+                        break;
+                    case AdvertiseCallback.ADVERTISE_FAILED_FEATURE_UNSUPPORTED:
+                        errorMessage += "not supported on this device.";
+                        break;
+                    case AdvertiseCallback.ADVERTISE_FAILED_INTERNAL_ERROR:
+                        errorMessage += "internal error.";
+                        break;
+                    case AdvertiseCallback.ADVERTISE_FAILED_TOO_MANY_ADVERTISERS:
+                        errorMessage += "too many advertisers.";
+                        break;
+                    case AdvertiserService.ADVERTISING_TIMED_OUT:
+                        errorMessage = "Advertising stopped due to timeout.";
+                        break;
+                    default:
+                        errorMessage += "unknown error";
+                }
+
+                Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_LONG).show();
+            }
+        };
+
+        mSwitch = (Switch) findViewById(R.id.switchAdvertisement);
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startAdvertising();
+                } else {
+                    stopAdvertising();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+
+                if (resultCode == RESULT_OK) {
+
+                    // Bluetooth is now Enabled, are Bluetooth Advertisements supported on this device?
+                    if (!mBluetoothAdapter.isMultipleAdvertisementSupported()) {
+
+                        // Bluetooth Advertisements are not supported.
+                        Toast.makeText(this, "Bluetooth Advertisements are not supported on this device.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                } else {
+
+                    // User declined to enable Bluetooth, exit the app.
+                    Toast.makeText(this, "User declined to enable Bluetooth, exiting Bluetooth Advertisements.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    /**
+     * When app comes on screen, check if BLE Advertisements are running, set switch accordingly,
+     * and register the Receiver to be notified if Advertising fails.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (AdvertiserService.running) {
+            mSwitch.setChecked(true);
+        } else {
+            mSwitch.setChecked(false);
+        }
+
+        IntentFilter failureFilter = new IntentFilter(AdvertiserService.ADVERTISING_FAILED);
+        registerReceiver(advertisingFailureReceiver, failureFilter);
+    }
+
+    /**
+     * When app goes off screen, unregister the Advertising failure Receiver to stop memory leaks.
+     * (and because the app doesn't care if Advertising fails while the UI isn't active)
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(advertisingFailureReceiver);
+    }
+
+    /**
+     * Starts BLE Advertising by starting {@code AdvertiserService}.
+     */
+    private void startAdvertising() {
+        Intent intent = new Intent(this, AdvertiserService.class);
+        startService(intent);
+    }
+
+    /**
+     * Stops BLE Advertising by stopping {@code AdvertiserService}.
+     */
+    private void stopAdvertising() {
+        Intent intent = new Intent(this, AdvertiserService.class);
+        stopService(intent);
+        mSwitch.setChecked(false);
+    }
+}
